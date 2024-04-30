59196faeb8811968ffa4881076ef383bf52bd492
dylanNicastro <145889356+dylanNicastro@users.noreply.github.com>
2024-04-30 05:24:12 +0000

    init_student_array completed

--- hw5.asm
+++ hw5.asm
@@ -35,6 +35,48 @@ print_student:
 	jr $ra 			# exit procedure
 	
 init_student_array:
+	move $t9, $a0		# amount of students to add stored in $t0
+	move $t1, $a1		# grab array of ids
+	move $t2, $a2		# grab array of credits
+	move $t3, $a3		# grab pointer to string of names
+	
+	
+	li $t4, 0		# current loop iteration
+	_init_loop:
+		beq $t9, $t4, _exit_init_loop	# exit loop if we reached the end of the students
+		sll $t5, $t4, 2			# calculate current index of integer arrays in bytes
+		
+		add $t6, $t5, $t1		# calculate byte index of current id
+		lw $a0, 0($t6)			# load id into $a0
+		
+		add $t6, $t5, $t2		# calculate byte index of current credits
+		lw $a1, 0($t6)			# load credits into $a1
+		
+		move $t8, $t3			# copy string into $t8
+		_get_name_loop:
+			lb $t7, 0($t8)			# load the current character from the string
+			beqz, $t7, _exit_name_loop	# if current character is null terminator, exit loop
+			addi $t8, $t8, 1			# increment pointer to the next character
+			j _get_name_loop
+			
+			
+		_exit_name_loop:
+		move $a2, $t3			# load address of string into $a2
+		move $t3, $t8			# "remove" that name from the string
+		addi $t3, $t3, 1		# increment pointer to the next character (past the null terminator)
+			
+		sll $t5, $t4, 3			# calculate current index of pointer
+		add $t6, $t5, $s0		# calculate address of current record
+		move $a3, $t6			# load address of current record into $a3
+		
+		move $t5, $ra
+		jal init_student
+		move $ra, $t5
+		
+		addi $t4, $t4, 1	# increment loop iteration
+		j _init_loop
+
+	_exit_init_loop:
 	jr $ra 			# exit procedure
 	
 insert:
